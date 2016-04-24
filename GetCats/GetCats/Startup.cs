b78deb930c6535d8a6459ef1781f7e2387b6acc9
Mattias <maze.89@hotmail.com>
2016-04-24 10:33:13 +0200

    Initial commit..

---
+++ GetCats/GetCats/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GetCats.Startup))]
+namespace GetCats
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
