edbe44f2f3c25f0fb1b4fbb90c37915809360361
Daniel Ryhle <daniel@ryhle.se>
2016-05-01 21:39:04 +0200

    init

---
+++ GetCats/GetCats/Controllers/WebAPI/TestController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Http;
+
+namespace GetCats.Controllers.WebAPI
+{
+    [Authorize]
+    public class TestController : ApiController
+    {
+        public string Get()
+        {
+            return "got here!";
+        }
+    }
+}
---
+++ GetCats/GetCats/Models/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using GetCats.Models.Entities;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace GetCats.Models
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext() : base("DefaultConnection", throwIfV1Schema: false) { }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
