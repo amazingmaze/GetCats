2e7f2d9ee6817209d1872e041fcab5c443aefc26
Mattias <maze.89@hotmail.com>
2016-05-06 10:31:37 +0200

    ..

--- GetCats/GetCats/Models/ViewModels/ImageViewModels.cs
+++ GetCats/GetCats/Models/ViewModels/ImageViewModels.cs
@@ -6,10 +6,12 @@ namespace GetCats.Models
 
     public class ImageViewModel
     {
+
         public int Id { get; set; }
         public string Title { get; set; }
         public string Src { get; set; }
         public double BuyOutPrice { get; set; }
         public double CurrentBid { get; set; }
+
     }
 }
