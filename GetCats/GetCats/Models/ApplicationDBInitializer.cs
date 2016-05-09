8b2989e3ec95faf17f9689fa9428999017f79251
Daniel Ryhle <daniel@ryhle.se>
2016-05-09 15:49:56 +0200

    fixed some smaller issues

--- GetCats/GetCats/Models/ApplicationDBInitializer.cs
+++ GetCats/GetCats/Models/ApplicationDBInitializer.cs
@@ -49,19 +49,6 @@ namespace GetCats.Models
                 Roles = {new IdentityUserRole {RoleId = admin.Id, UserId = user.Id}},
             };
 
-            //DUMMY IMAGES, THESE WONT WORK TO VIEW!!!
-            var img1 = new Image { FileName = "baba1.jpg", Name = "Some baba cat!", Options = new PurchaseOption[]
-            {
-                new PurchaseOption { Resolution = PurchaseOption.ImageResolution.High, Price = 11, Id = Guid.Parse("7305926d-15f5-4adc-820d-ab9443acf963")},
-                new PurchaseOption { Resolution = PurchaseOption.ImageResolution.Low, Price = 6, Id = Guid.Parse("e035e7a4-56a9-48e1-9b4f-157f21dbbe51") }
-            }};
-            var img2 = new Image { FileName = "baba1.jpg", Name = "1337 cat", Options = new PurchaseOption[] {
-                new PurchaseOption { Resolution = PurchaseOption.ImageResolution.High, Price = 11, Id = Guid.Parse("5afff234-e76c-477f-8263-810020a1ef6d") },
-                new PurchaseOption { Resolution = PurchaseOption.ImageResolution.Low, Price = 6, Id = Guid.Parse("899012aa-2b03-4eb8-b283-4f897aa9f1a0") }
-            }};
-
-            dbContext.Images.AddRange(new List<Image> {img1, img2});
-
             dbContext.Roles.Add(admin); //create admin role
             dbContext.Roles.Add(user); //Create user role
             dbContext.Users.Add(newAdmin);
